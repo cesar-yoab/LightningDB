864ca05c004259a95773a0c42ffe909f14de437d
Cesar <50643126+cesar-yoab@users.noreply.github.com>
2023-02-18 21:21:42 +0000

    Started working on the TCP server

---
+++ lightningdb-cli.py
@@ -0,0 +1,40 @@
+import socket
+
+HOST = "127.0.0.1"
+PORT = 6379
+
+def main():
+    """Creates the console and connects to the database"""
+    # Connect to the server
+    s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    s.connect((HOST, PORT))
+
+    # Print initial ascii art
+    print("="*10)
+    print("LightningDB")
+    print("|\\---/|")
+    print("| o_o |")
+    print(" \\_^_/")
+    print("="*10)
+    print("> ", end="")
+
+    # Read commands from the user and send them to the server
+    while True:
+        # Read a command from the user
+        command = input()
+        if command == "EXIT":
+            s.close()
+            return
+
+        # Send the command to the server
+        s.sendall(command.encode())
+
+        # Receive and display the server's response
+        response = s.recv(1024)
+        print(response.decode(), end="")
+
+        # Print the prompt again
+        print("> ", end="")
+
+if __name__ == '__main__':
+    main()
---
+++ lightningdb/src/main.rs
@@ -0,0 +1,47 @@
+use std::io::{Read, Write};
+use std::net::{TcpListener, TcpStream};
+use std::thread;
+
+use lightningdb::ThreadPool;
+
+fn handle_client(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    loop {
+        match stream.read(&mut buffer) {
+            Ok(size) => {
+                if size == 0 {
+                    break;
+                }
+
+                let message = String::from_utf8_lossy(&buffer[..size]);
+                println!("Received message: {}", message);
+                stream.write_all(b"Message received\n").unwrap();
+            }
+            Err(e) => {
+                println!("Error reading from socket: {}", e);
+                break;
+            }
+        }
+    }
+}
+
+fn main() {
+    let listener = TcpListener::bind("127.0.0.1:6379").unwrap();
+    let pool = ThreadPool::new(4);
+    println!("Server listening on port 6379");
+
+    for stream in listener.incoming() {
+        match stream {
+            Ok(stream) => {
+                println!("New client connected: {}", stream.peer_addr().unwrap());
+                pool.execute(move || {
+                    handle_client(stream);
+                });
+            }
+            Err(e) => {
+                println!("Error connecting to client: {}", e);
+            }
+        }
+    }
+}
